9012a610428a975d33c169b5958ce1261e4bffc4
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-23 04:52:30 +0100

    feat(admin): Initial admin executable source code

---
+++ Program.cs
@@ -0,0 +1,107 @@
+/// <summary>Launch the shortcut's target PowerShell script with the markdown.</summary>
+/// <version>0.0.1.0</version>
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.VisualBasic;
+
+namespace cvmd2html
+{
+  static class Program
+  {
+    static void Main()
+    {
+      RequestAdminPrivileges();
+
+      /** The application execution. */
+      if (!string.IsNullOrEmpty(Parameters.Markdown))
+      {
+        const string CMD_EXE = @"C:\Windows\System32\cmd.exe";
+        const string CMD_LINE_FORMAT = @"/d /c """"{0}"" 2> ""{1}""""";
+        Package.IconLink.Create(Parameters.Markdown);
+        if (WaitForExit(Process.Start(
+          new ProcessStartInfo
+          {
+            FileName = CMD_EXE,
+            Arguments = string.Format(CMD_LINE_FORMAT, Package.IconLink.Path, ErrorLog.Path),
+            WindowStyle = ProcessWindowStyle.Hidden
+          }
+        ).Id) != 0)
+        {
+          ErrorLog.Read();
+          ErrorLog.Delete();
+        }
+        Package.IconLink.Delete();
+        Quit(0);
+      }
+
+      /** Configuration and settings. */
+      if (Parameters.Set ^ Parameters.Unset)
+      {
+        if (Parameters.Set)
+        {
+          Setup.Set();
+          if ((bool)Parameters.NoIcon) Setup.RemoveIcon();
+          else Setup.AddIcon();
+        }
+        else Setup.Unset();
+        Quit(0);
+      }
+
+      Quit(1);
+    }
+
+    /// <summary>The path to the application.</summary>
+    internal static readonly string Path = Assembly.GetExecutingAssembly().Location;
+
+    /// <summary>Wait for the process exit.</summary>
+    /// <param name="processId">The process identifier.</param>
+    /// <returns>The exit status of the process.</returns>
+    static int WaitForExit(int processId)
+    {
+      // The process termination event query. Win32_ProcessStopTrace requires admin rights to be used.
+      var wmiQuery = "SELECT * FROM Win32_ProcessStopTrace WHERE ProcessName='cmd.exe' AND ProcessId=" + processId;
+      // Wait for the process to exit.
+      dynamic watcher = Util.GetObject().ExecNotificationQuery(wmiQuery);
+      dynamic cmdProcess = watcher.NextEvent();
+      try
+      {
+        return cmdProcess.ExitStatus;
+      }
+      finally
+      {
+        Util.ReleaseComObject(ref cmdProcess);
+        Util.ReleaseComObject(ref watcher);
+      }
+    }
+
+    /// <summary>Request administrator privileges.</summary>
+    static void RequestAdminPrivileges()
+    {
+      if (IsCurrentProcessElevated()) return;
+      dynamic shell = Util.CreateObject("Shell.Application");
+      shell.ShellExecute(Path, Interaction.Command(), Missing.Value, "runas", Constants.vbHidden);
+      Util.ReleaseComObject(ref shell);
+      Quit(0);
+    }
+
+    /// <summary>Check if the process is elevated.</summary>
+    /// <returns>True if the running process is elevated, false otherwise.</returns>
+    static bool IsCurrentProcessElevated()
+    {
+      const int HKU = unchecked((int)0x80000003);
+      Util.Registry.CheckAccess(HKU, @"S-1-5-19\Environment", Missing.Value, out bool bGranted);
+      return bGranted;
+    }
+
+    /// <summary>Clean up and quit.</summary>
+    /// <param name="exitCode">The exit code.</param>
+    internal static void Quit(int exitCode)
+    {
+      Util.Dispose();
+      GC.Collect();
+      Environment.Exit(exitCode);
+    }
+  }
+}
---
+++ src/Setup.cs
@@ -0,0 +1,42 @@
+/// <summary>The methods for managing the shortcut menu option: install and uninstall.</summary>
+/// <version>0.0.1.0</version>
+
+using Microsoft.Win32;
+
+namespace cvmd2html
+{
+  static class Setup
+  {
+    const string SHELL_SUBKEY = @"SOFTWARE\Classes\SystemFileAssociations\.md\shell";
+    const string VERB = "cthtml";
+    static readonly string VERB_SUBKEY = string.Format(@"{0}\{1}", SHELL_SUBKEY, VERB);
+    static readonly string VERB_KEY = string.Format(@"{0}\{1}", Registry.CurrentUser, VERB_SUBKEY);
+    const string ICON_VALUENAME = "Icon";
+
+    /// <summary>Configure the shortcut menu in the registry.</summary>
+    internal static void Set()
+    {
+      string COMMAND_KEY = VERB_KEY + @"\command";
+      var command = string.Format(@"""{0}"" /Markdown:""%1""", Program.Path);
+      Registry.SetValue(COMMAND_KEY, null, command);
+      Registry.SetValue(VERB_KEY, null, "Convert to &HTML");
+    }
+
+    /// <summary>Add an icon to the shortcut menu in the registry.</summary>
+    internal static void AddIcon() => Registry.SetValue(VERB_KEY, ICON_VALUENAME, Program.Path);
+
+    /// <summary>Remove the shortcut icon menu.</summary>
+    internal static void RemoveIcon()
+    {
+      using RegistryKey VERB_KEY_OBJ = Registry.CurrentUser.OpenSubKey(VERB_SUBKEY, true);
+      VERB_KEY_OBJ?.DeleteValue(ICON_VALUENAME, false);
+    }
+
+    /// <summary>Remove the shortcut menu by removing the verb key and subkeys.</summary>
+    internal static void Unset()
+    {
+      using RegistryKey SHELL_KEY_OBJ = Registry.CurrentUser.OpenSubKey(SHELL_SUBKEY, true);
+      SHELL_KEY_OBJ?.DeleteSubKeyTree(VERB, false);
+    }
+  }
+}
