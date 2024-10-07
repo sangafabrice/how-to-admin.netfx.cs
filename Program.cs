e386a2ef0ae9d9bf9eb634dd7adfa80917f826ea
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-07 02:38:01 +0100

    feat: Use CLR types for checking elevation

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 /// <summary>Launch the shortcut's target PowerShell script with the markdown.</summary>
-/// <version>0.0.1.4</version>
+/// <version>0.0.1.5</version>
 
 using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.ComponentModel;
-using WbemScripting;
-using ROOT.CIMV2;
-
-[assembly: AssemblyTitle("CvMd2Html")]
+using System.Security.Principal;
+using System.Management;
 
 namespace cvmd2html
 {
@@ -71,30 +69,12 @@ namespace cvmd2html
     /// <summary>Wait for the process exit.</summary>
     /// <param name="processId">The process identifier.</param>
     /// <returns>The exit status of the process.</returns>
-    static int WaitForExit(int processId)
+    static uint WaitForExit(int processId)
     {
       // The process termination event query. Win32_ProcessStopTrace requires admin rights to be used.
       var wmiQuery = "SELECT * FROM Win32_ProcessStopTrace WHERE ProcessName='cmd.exe' AND ProcessId=" + processId;
       // Wait for the process to exit.
-      var wbemLocator = new SWbemLocator();
-      SWbemServices wmiService = wbemLocator.ConnectServer();
-      SWbemEventSource watcher = wmiService.ExecNotificationQuery(wmiQuery);
-      SWbemObject cmdProcess = watcher.NextEvent();
-      SWbemPropertySet properties = cmdProcess.Properties_;
-      SWbemProperty ExitStatus = properties.Item("ExitStatus");
-      try
-      {
-        return (int)ExitStatus.get_Value();
-      }
-      finally
-      {
-        _.ReleaseComObject(ref ExitStatus);
-        _.ReleaseComObject(ref properties);
-        _.ReleaseComObject(ref cmdProcess);
-        _.ReleaseComObject(ref watcher);
-        _.ReleaseComObject(ref wmiService);
-        _.ReleaseComObject(ref wbemLocator);
-      }
+      return (uint)new ManagementEventWatcher(wmiQuery).WaitForNextEvent().Properties["ExitStatus"].Value;
     }
 
     /// <summary>Request administrator privileges.</summary>
@@ -128,10 +108,7 @@ namespace cvmd2html
     /// <returns>True if the running process is elevated, false otherwise.</returns>
     static bool IsCurrentProcessElevated()
     {
-      const uint HKU = 0x80000003;
-      bool bGranted;
-      StdRegProv.CheckAccess(HKU, @"S-1-5-19\Environment", out bGranted);
-      return bGranted;
+      return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
     }
 
     /// <summary>Clean up and quit.</summary>
