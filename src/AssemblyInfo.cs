a07f84eb84823047a8c5e195ac44aa6e8ce35ddb
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-07 02:38:01 +0100

    feat: Use CLR types for checking elevation

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,5 +3,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("CvMd2Html Shortcut")]
 [assembly: AssemblyCopyright("© 2024 sangafabrice")]
 [assembly: AssemblyCompany("sangafabrice")]
-[assembly: AssemblyInformationalVersion("0.0.1.141")]
-[assembly: AssemblyVersion("0.0.1.141")]
+[assembly: AssemblyTitle("CvMd2Html")]
+[assembly: AssemblyInformationalVersion("0.0.1.300")]
+[assembly: AssemblyVersion("0.0.1.300")]
