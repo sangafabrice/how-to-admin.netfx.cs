0d48ffbbbd7838bd9d60106f5b9f20fb3db89d29
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-05 21:54:17 +0100

    refactor: Remove dynamic shell and add STA attribute

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("© 2024 sangafabrice")]
 [assembly: AssemblyCompany("sangafabrice")]
 [assembly: AssemblyTitle("CvMd2Html")]
-[assembly: AssemblyInformationalVersion("0.0.1.5")]
-[assembly: AssemblyVersion("0.0.1.5")]
+[assembly: AssemblyInformationalVersion("0.0.1.6")]
+[assembly: AssemblyVersion("0.0.1.6")]
