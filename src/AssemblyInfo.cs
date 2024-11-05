d4ddfbfac6c00fda7a0518275b79c7bcf2059eb2
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-05 22:47:25 +0100

    feat: Create the StdRegProv WMI class

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("CvMd2Html Shortcut")]
 [assembly: AssemblyCopyright("© 2024 sangafabrice")]
 [assembly: AssemblyCompany("sangafabrice")]
-[assembly: AssemblyTitle("CvMd2Html")]
-[assembly: AssemblyInformationalVersion("0.0.1.6")]
-[assembly: AssemblyVersion("0.0.1.6")]
+[assembly: AssemblyInformationalVersion("0.0.1.134")]
+[assembly: AssemblyVersion("0.0.1.134")]
