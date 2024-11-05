3f6cdc5849b41d5b9b68ab9e1fd3826644d8c511
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-05 21:54:17 +0100

    refactor: Convert late-binding to early-binding

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 1.0.0#>
+<#PSScriptInfo .VERSION 1.0.1#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -23,11 +23,12 @@ param ()
   New-Item $BinDir -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
   Copy-Item "$PSScriptRoot\rsc" -Destination $BinDir -Recurse
   Save-ProjectPackage $PSScriptRoot
+  Set-ProjectVersion $PSScriptRoot
 
   # Compile the source code with csc.exe.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\Roslyn\;$Env:Path"
-  csc.exe /nologo /langversion:latest /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:System.Xaml.dll /reference:Microsoft.VisualBasic.dll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.cs" "$SrcDir\ErrorLog.cs" "$SrcDir\Package.cs" "$SrcDir\Parameters.cs" "$PSScriptRoot\Program.cs" "$SrcDir\Setup.cs" "$SrcDir\Util.cs"
+  csc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /reference:"$BinDir\Interop.WbemScripting.dll" /reference:"$BinDir\Interop.Shell32.dll" /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:System.Xaml.dll /reference:Microsoft.VisualBasic.dll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.cs" "$SrcDir\ErrorLog.cs" "$SrcDir\Package.cs" "$SrcDir\Parameters.cs" "$PSScriptRoot\Program.cs" "$SrcDir\Setup.cs" "$SrcDir\Util.cs"
   $Env:Path = $EnvPath
 
   if ($LASTEXITCODE -eq 0) {
