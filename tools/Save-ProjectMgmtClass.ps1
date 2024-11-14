ae94a009ccb96e2dbf09f8297a5890cda36638e6
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-14 08:37:37 +0100

    refactor: Create an mgmt inspired StdRegProv class

--- tools/Save-ProjectMgmtClass.ps1
+++ tools/Save-ProjectMgmtClass.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 1.0.0#>
+<#PSScriptInfo .VERSION 1.0.1#>
 
 [CmdletBinding()]
 param (
@@ -10,6 +10,6 @@ param (
 $FileName = $ClassName.Replace('_', '.')
 $EnvPath = $Env:Path
 $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
-csc.exe /nologo /target:library /out:$(($ClassDll = "$DirName\$FileName.dll")) /reference:"$DirName\Interop.WbemScripting.dll" "$(($SrcDir = "$Root\src"))\AssemblyInfo.cs" "$SrcDir\$FileName.cs"
+csc.exe /nologo /target:library /out:$(($ClassDll = "$DirName\$FileName.dll")) "$(($SrcDir = "$Root\src"))\AssemblyInfo.cs" "$SrcDir\$FileName.cs"
 $Env:Path = $EnvPath
 return $ClassDll
